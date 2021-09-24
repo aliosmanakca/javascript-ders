4f1e0fdaf2dc68771555dcec73452bb87c91642c
aliosmanakca <akcaaliosman@gmail.com>
2021-09-24 12:34:49 -0400

    .

---
+++ ders02-karar/app.js
@@ -0,0 +1,19 @@
+//==KARAR YAPILARI==
+
+//===IF-ELSE===
+
+const s1 = Number(prompt("birinci sayi"));
+const islem = prompt("işlemi giriniz");
+const s2 = Number(prompt("ikinci sayi"));
+let sonuc = 0;
+
+if (islem == "+") {
+  sonuc = s1 + s2;
+} else if (islem == "-") {
+  sonuc = s1 - s2;
+} else if (islem == "*") {
+  sonuc = s1 * s2;
+} else if (islem == "/") {
+  sonuc = s1 / s2;
+}
+console.log(sonuc);
